bf07e8663728d425a82c753618cc8ab2eee72673
Darsan <lankagames1991@gmail.com>
2016-05-21 12:17:57 +0530

    Add /todos and /todos/:id

--- saver.js
+++ saver.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT||3000;
+var todos = [{id:1,description:"Meet friend",completed:false},
+            {id:2,description:"Meet Girl Friend",completed:false}];
 
 app.get('/',function(req,res)
        {
@@ -8,6 +10,34 @@ app.get('/',function(req,res)
     
 });
 
+app.get('/todos',function(req,res){
+    res.json(todos);
+    
+});
+
+app.get('/todos/:id',function(req,res)
+{
+    var findObj;
+    todos.forEach(function(obj){
+        
+        if(obj.id == req.params.id)
+        {
+            findObj = obj;
+            return;
+        }
+        
+    });
+
+    res.json(findObj);
+});
+
 app.listen(PORT,function(){
     console.log("Server Start");
 });
+
+
+function GetObjById(id)
+{
+    
+
+}
